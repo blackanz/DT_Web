77688e58d952dfae413e5d6d0cc366752a60b95b
bulb <blackanz@users.noreply.github.com>
2026-03-03 10:07:37 +0900

    orderStore update

--- src/pages/Kiosk/VoiceOrderView.vue
+++ src/pages/Kiosk/VoiceOrderView.vue
@@ -6,7 +6,7 @@ const fakeSTT = () => store.setVoiceText('아메리카노 한 잔');
 
 <template>
   <h1>음성 주문</h1>
-  <button @click="fakeSTT">🎤 음성 인식</button>
+  <button @click="fakeSTT">음성 인식</button>
   <p>{{ store.voiceText }}</p>
   <button @click="$router.push('/confirm')">주문 확인</button>
 </template>
